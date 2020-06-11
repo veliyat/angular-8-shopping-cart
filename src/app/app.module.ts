cf2bb543478d2e7d51029c9c465189aabc695acd
Ravi Veliyat <veliyat@gmail.com>
2020-06-11 23:04:49 +0530

    Part 8 : Register Form (Reactive Form)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module'
 
@@ -39,7 +39,8 @@ import { PageNotFoundComponent } from './components/shared/page-not-found/page-n
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+
+/**
+ *
+ * @param form
+ */
+
+function passwordsMatchValidator(form) {
+  const password = form.get('password')
+  const confirmPassword = form.get('confirmPassword')
+
+  if(password.value !== confirmPassword.value) {
+    confirmPassword.setErrors({ passwordsMatch: true })
+  } else {
+    confirmPassword.setErrors(null)
+  }
+
+  return null
+}
+
+/**
+ * If the data is valid return null else return an object
+ */
+function symbolValidator(control) { //control = registerForm.get('password')
+  if(control.hasError('required')) return null;
+  if(control.hasError('minlength')) return null;
+
+  if(control.value.indexOf('@') > -1) {
+    return null
+  } else {
+    return { symbol: true }
+  }
+}
 
 @Component({
   selector: 'app-register',
@@ -7,9 +40,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(private builder: FormBuilder) { }
 
   ngOnInit() {
+    this.buildForm()
+  }
+
+  buildForm() {
+    this.registerForm = this.builder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, symbolValidator, Validators.minLength(4)]],
+      confirmPassword: ''
+    }, {
+      validators: passwordsMatchValidator
+    })
+  }
+
+  register() {
+    console.log(this.registerForm.value)
   }
 
 }
